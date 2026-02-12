b6f23048b7ee1b9603c70ec1a39e5074b70c23e6
pulsar <pulsarmn@yandex.ru>
2026-02-12 14:35:59 +0300

    feat(auth): add a User entity for JPA
    - Add Lombok Gradle plugin
    - Add jakarta-persistence-api

---
+++ auth-service/src/main/java/org/odnokursniki/auth/domain/User.java
@@ -0,0 +1,42 @@
+package org.odnokursniki.auth.domain;
+
+
+import lombok.*;
+import jakarta.persistence.*;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.UUID;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(schema = "public", name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "display_name")
+    private String displayName;
+
+    @Column(name = "bio")
+    private String bio;
+
+    @Column(name = "birthdate")
+    private LocalDate birthdate;
+
+    @Builder.Default
+    @Column(name = "created_at")
+    private Instant createdAt = Instant.now();
+}
