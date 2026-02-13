c2e8205b470fc57a2f86c03f7c65cbd7be24d50d
pulsar <pulsarmn@yandex.ru>
2026-02-13 12:32:57 +0300

    docs(auth): write JavaDoc for CodeGenerator classes

--- auth-service/src/main/java/org/odnokursniki/auth/util/CodeGenerator.java
+++ auth-service/src/main/java/org/odnokursniki/auth/util/CodeGenerator.java
@@ -1,6 +1,17 @@
 package org.odnokursniki.auth.util;
 
+
+/**
+ * An interface that defines operations for code generation
+ *
+ * @since 0.1.0
+ * @author pulsar
+ */
 public interface CodeGenerator {
 
+    /**
+     * Generates some code and returns it
+     * @return non {@code null} code
+     */
     String generate();
 }
--- auth-service/src/main/java/org/odnokursniki/auth/util/SimpleCodeGenerator.java
+++ auth-service/src/main/java/org/odnokursniki/auth/util/SimpleCodeGenerator.java
@@ -6,12 +6,23 @@ import org.springframework.stereotype.Component;
 import java.util.Random;
 
 
+/**
+ * {@link org.odnokursniki.auth.util.CodeGenerator} default implementation
+ * </br>
+ *
+ * @since 0.1.0
+ * @author pulsar
+ */
 @Slf4j
 @Component
 public class SimpleCodeGenerator implements CodeGenerator {
 
     private final Random random = new Random();
 
+    /**
+     * <p>Generates a random six-digit code
+     * @return six-digit code
+     */
     @Override
     public String generate() {
         int randomCode = random.nextInt(100_000, 999_999);
